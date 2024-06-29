5f5f606b875436dd8ec29d6c5d4adec4ce97f40d
VainiChauhan <vainichauhan@gmail.com>
2024-06-29 20:40:00 +0530

    commit

---
+++ CodePilot/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CodePilot.Models;
+
+namespace CodePilot.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<CodeSubmission> CodeSubmissions { get; set; }
+    }
+}
---
+++ CodePilot/Models/Question.cs
@@ -0,0 +1,12 @@
+﻿namespace CodePilot.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string SampleInput { get; set; }
+        public string SampleOutput { get; set; }
+        public string Language { get; set; }
+    }
+}
